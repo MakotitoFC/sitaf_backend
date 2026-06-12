bff246478bb78ff8fa423f401cdc7101af8b84c4
MakotitoFC <makotitofc@gmail.com>
2026-06-12 00:30:32 -0500

    feat:Creación de entorno

---
+++ src/main/java/com/sitaf/backend/SitafBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sitaf.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitafBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitafBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitaf/backend/SitafBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitaf.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitafBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
